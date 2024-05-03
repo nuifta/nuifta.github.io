8e6eb7cf62fc3ddb01043854918999e6103c8ac6
nuifta <nuifta@gmail.com>
2024-05-02 21:52:32 -0400

    Some Changes

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo off
 set NODE_SKIP_PLATFORM_CHECK=1
+pushd "D:\Proyectos HTML"
 .\tailwindcss-windows-x64.exe init
 .\tailwindcss-windows-x64.exe -i ./src/input.css -o ./src/output.css --watch
 .\tailwindcss-windows-x64.exe -i ./src/input.css -o ./src/output.css --minify
